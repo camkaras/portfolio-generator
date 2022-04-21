04894511a5220e95d2528082997c7b13f6200669
Cameron <cameron.karasiuk@gmail.com>
2022-04-21 18:18:09 -0400

    generating webpage

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const generatePage = require('./src/page-template.js');
 
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++){
-        console.log(profileDataArr);
-    }
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
 
 
 
-    console.log('================');
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-    //IS the same as this...
-
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-        
-    });
-};
-
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
